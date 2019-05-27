0cedaab6861646cd034187c8a3679f56379db138
xtrp <36894883+xtrp@users.noreply.github.com>
2019-05-28 00:50:51 +0100

    updates

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
 document.onkeydown = function(e){
-	var audio = new Audio(chrome.extension.getURL("https://freesound.org/data/previews/439/439559_9018380-lq.mp3"));
+	var audio = new Audio(chrome.extension.getURL("https://xtrp.github.io/MoanKeyboardAssets/" + Math.floor(Math.random() * 6).toString() + ".mp3"));
 	audio.play();
 }
