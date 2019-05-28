2b6752acea5f68bf4a48d57f71c547ff87d95963
xtrp <36894883+xtrp@users.noreply.github.com>
2019-05-28 10:39:09 +0100

    updates

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
 document.onkeydown = function(e){
-	var audio = new Audio(chrome.extension.getURL("https://xtrp.github.io/MoanKeyboardAssets/" + Math.floor(Math.random() * 6).toString() + ".mp3"));
+	var audio = new Audio(chrome.extension.getURL("https://xtrp.github.io/MoanKeyboardAssets/" + Math.floor(Math.random() * 6).toString() + ".m4a"));
 	audio.play();
 }
