0b3323f1e0c8320de79868551246c519aa907ba6
xtrp <36894883+xtrp@users.noreply.github.com>
2019-05-28 10:54:33 +0100

    done

--- popup.js
+++ popup.js
@@ -1,4 +1,19 @@
-document.onkeydown = function(e){
+// ALL SOUNDS IN THIS CHROME EXTENSION ARE FROM FREESOUND.ORG
+
+var allAudioFiles = [];
+
+document.onkeydown = event;
+document.onclick = event;
+function event(){
 	var audio = new Audio(chrome.extension.getURL("https://xtrp.github.io/MoanKeyboardAssets/" + Math.floor(Math.random() * 6).toString() + ".m4a"));
-	audio.play();
+	allAudioFiles.push(audio);
+	if(allAudioFiles.length > 1) {
+		allAudioFiles.forEach(function(audioFile,index){
+			if(index < allAudioFiles.length - 1){
+				allAudioFiles[index].pause();
+			}
+		});
+		allAudioFiles.splice(0, allAudioFiles.length - 1);
+	}
+	allAudioFiles[allAudioFiles.length - 1].play();
 }
